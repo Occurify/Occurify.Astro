cf8fa4c99b91a8fe7e1eb1b12fd2699318035000
Jasper <jasper.lammers@gmail.com>
2025-03-13 12:05:24 +0100

    Updated implementation, tests, target frameworks and package information.

---
+++ Occurify.Astro/Occurify.Astro/Coordinates.cs
@@ -0,0 +1,29 @@
+﻿namespace Occurify.Astro;
+
+/// <summary>
+/// Represents a geographic coordinate with latitude, longitude, and optional height.
+/// </summary>
+/// <param name="Latitude">The latitude in degrees.</param>
+/// <param name="Longitude">The longitude in degrees.</param>
+/// <param name="Height">Height in meter relative to the horizon (default is 0).</param>
+public record Coordinates(double Latitude, double Longitude, double Height = 0)
+{
+    private static Coordinates? _local;
+
+    /// <summary>
+    /// The local <see cref="Coordinates"/> object used by methods that don't take a <see cref="Coordinates"/> object.
+    /// Note that this is not set by default.
+    /// </summary>
+    public static Coordinates Local
+    {
+        get
+        {
+            if (_local == null)
+            {
+                throw new InvalidOperationException("Local Coordinates not set.");
+            }
+            return _local;
+        }
+        set => _local = value;
+    }
+}
---
+++ Occurify.Astro/Occurify.Astro/SunPhases.cs
@@ -0,0 +1,21 @@
+﻿namespace Occurify.Astro;
+
+[Flags]
+public enum SunPhases
+{
+    None = 0,
+    SolarNoon = 1 << 0,
+    Nadir = 1 << 1,
+    Sunrise = 1 << 2,
+    Sunset = 1 << 3,
+    SunriseEnd = 1 << 4,
+    SunsetStart = 1 << 5,
+    Dawn = 1 << 6,
+    Dusk = 1 << 7,
+    NauticalDawn = 1 << 8,
+    NauticalDusk = 1 << 9,
+    NightEnd = 1 << 10,
+    Night = 1 << 11,
+    GoldenHourEnd = 1 << 12,
+    GoldenHour = 1 << 13
+}
